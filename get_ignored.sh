a4520f3cc98cba6733ce16491be6cd9f79c552cd
Brian Reese <brian.reese@gmail.com>
2018-04-07 01:12:06 -0400

    Using variables for git targets.

--- get_ignored.sh
+++ get_ignored.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-tags=()
+git_remote='origin'
+if [ -n "$heimdall_git_remote" ]; then
+  git_remote="$heimdall_git_remote"
+fi
 
+git_branch='master'
+if [ -n "$heimdall_git_branch" ]; then
+  git_branch="$heimdall_git_branch"
+fi
+
+tags=()
 get_tags() {
   tags=("${tags[@]}" $(git notes show $1 | awk '/^heimdall_ignore/ {print $2}'))
 }
 
-for COMMIT in `git rev-list github/master..HEAD`; do 
+for COMMIT in `git rev-list "$git_remote"/"$git_branch"..HEAD`; do 
   get_tags $COMMIT 2>/dev/null
 done
 
